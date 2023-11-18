9ac19f6d3008f839eaf1b673a1fb351b132624bc
hendrckks <samueltetenga@gmail.com>
2023-11-18 16:48:16 +0300

    first commit

---
+++ optimum/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <div className="p-3">
+      <h1 className="font-bold">
+        <span className="text-teal-700">Optimum</span>
+        <span className="text-neutral-600">Outreach</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ optimum/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="px-3">Home</div>;
+}
+
+export default Home;
