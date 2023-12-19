865e5bddb857120c0766b0dccd8cfff4ae1db2f6
hendrckks <samueltetenga@gmail.com>
2023-12-19 12:53:13 +0300

    faq further styling

--- optimum/src/components/FAQs.tsx
+++ optimum/src/components/FAQs.tsx
@@ -6,7 +6,7 @@ import RemoveIcon from "@mui/icons-material/Remove";
 function FAQs() {
   const [selected, setSelected] = useState(null);
 
-  const toggle = (index) => {
+  const toggle = (index: number) => {
     if (selected === index) {
       return setSelected(null);
     }
@@ -31,22 +31,24 @@ function FAQs() {
   ];
 
   return (
-    <div className="bg-[#1D1A27] h-[100vh] font-DM-sans">
+    <div className="faq-wrapper">
       <div className="p-24 flex flex-col justify-center items-center">
-        <h1 className="text-[50px] font-semibold">All The A's For Your Q's</h1>
-        <div className="flex flex-col justify-center items-center w-[520px] mt-16">
+        <h1 className="text-[50px] font-semibold text-black">
+          All The A's For Your Q's
+        </h1>
+        <div className="flex flex-col justify-center items-center w-[720px] mt-16">
           {AccordionData.map((data, index) => (
             <div key={index} className="m-auto cursor-pointer">
               <div
                 className="flex items-center py-3"
                 onClick={() => toggle(index)}
               >
-                <div className="flex-col bg-slate-300 p-8 rounded-xl cursor-pointer transform hover:translate-y-1 transition-transform duration-200 ease-in">
+                <div className="flex-col bg-[#1d1a27] text-white p-10 rounded-[22px] cursor-pointer transform hover:translate-y-1 transition-transform duration-300 ease-in inset-0 delay-0 shadow-md">
                   <div className="flex items-center justify-between">
-                    <h2 className="font-semibold text-[20px] flex items-center ">
+                    <h2 className="font-semibold text-[18px] flex items-center ">
                       {data.title}
                     </h2>
-                    <div className="text-[20px] flex items-centertransition-all ease-in-out 0.5s">
+                    <div className="text-[24px] flex items-center transition-shadow transform ease-in-out duration-300">
                       {selected == index ? <RemoveIcon /> : <AddIcon />}
                     </div>
                   </div>
@@ -54,10 +56,10 @@ function FAQs() {
                     className={
                       selected == index
                         ? "max-h-[999px]"
-                        : "max-h-0 transition-all 0.5s cubic-bezier(0,1,0,1) overflow-hidden"
+                        : "max-h-0 transition-shadow duration-300 cubic-bezier(0,1,0,1) overflow-hidden ease-in-out"
                     }
                   >
-                    <p className="flex items-center justify-center overflow-auto mt-4">
+                    <p className="flex items-center justify-center overflow-auto mt-4 text-neutral-400">
                       {data.Description}
                     </p>
                   </div>
