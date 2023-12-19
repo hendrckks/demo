a4065906bbaec2ba304cf2f647b19473c63b5462
hendrckks <samueltetenga@gmail.com>
2023-12-19 14:05:59 +0300

    style

--- optimum/src/components/FAQs.tsx
+++ optimum/src/components/FAQs.tsx
@@ -33,7 +33,7 @@ function FAQs() {
   return (
     <div className="faq-wrapper">
       <div className="p-24 flex flex-col justify-center items-center">
-        <h1 className="text-[50px] font-semibold text-black">
+        <h1 className="text-[50px] font-semibold text-white">
           All The A's For Your Q's
         </h1>
         <div className="flex flex-col justify-center items-center w-[720px] mt-16">
@@ -43,13 +43,17 @@ function FAQs() {
                 className="flex items-center py-3"
                 onClick={() => toggle(index)}
               >
-                <div className="flex-col bg-[#1d1a27] text-white p-10 rounded-[22px] cursor-pointer transform hover:translate-y-1 transition-transform duration-300 ease-in inset-0 delay-0 shadow-md">
+                <div className="flex-col bg-[#d8962e] text-black p-10 rounded-[22px] cursor-pointer transform hover:translate-y-1 transition-transform duration-300 ease-in inset-0 delay-0 shadow-md">
                   <div className="flex items-center justify-between">
-                    <h2 className="font-semibold text-[18px] flex items-center ">
+                    <h2 className="font-semibold text-[20px] flex items-center ">
                       {data.title}
                     </h2>
-                    <div className="text-[24px] flex items-center transition-shadow transform ease-in-out duration-300">
-                      {selected == index ? <RemoveIcon /> : <AddIcon />}
+                    <div className="flex items-center transition-shadow transform ease-in-out duration-300">
+                      {selected == index ? (
+                        <RemoveIcon className="text-[30px]" />
+                      ) : (
+                        <AddIcon className="text-[30px]" />
+                      )}
                     </div>
                   </div>
                   <div
@@ -59,7 +63,7 @@ function FAQs() {
                         : "max-h-0 transition-shadow duration-300 cubic-bezier(0,1,0,1) overflow-hidden ease-in-out"
                     }
                   >
-                    <p className="flex items-center justify-center overflow-auto mt-4 text-neutral-400">
+                    <p className="flex items-center justify-center overflow-auto mt-4 text-neutral-900">
                       {data.Description}
                     </p>
                   </div>
